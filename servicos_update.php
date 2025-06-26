f237c55308ef0520d27c8eee090ef57749f67839
Hymdell <cristopherprofissional@gmail.com>
2025-06-26 09:02:54 -0300

    correção atualização serviços

--- servicos_update.php
+++ servicos_update.php
@@ -2,10 +2,19 @@
 // servicos_update.php - Atualiza um serviço existente
 require 'config.php';
 
-$id = $_POST['id'] ?? '';
-$nome = $_POST['nome'] ?? '';
-$valor = $_POST['valor'] ?? '';
-$comissao = $_POST['comissao'] ?? '';
+// Permitir tanto POST tradicional quanto JSON
+if ($_SERVER['CONTENT_TYPE'] === 'application/json') {
+    $data = json_decode(file_get_contents('php://input'), true);
+    $id = $data['id'] ?? '';
+    $nome = $data['nome'] ?? '';
+    $valor = $data['valor'] ?? '';
+    $comissao = $data['comissao'] ?? '';
+} else {
+    $id = $_POST['id'] ?? '';
+    $nome = $_POST['nome'] ?? '';
+    $valor = $_POST['valor'] ?? '';
+    $comissao = $_POST['comissao'] ?? '';
+}
 
 if ($id && $nome && $valor && $comissao) {
     $stmt = $pdo->prepare('UPDATE Servicos SET nome=?, valor=?, comissao=? WHERE id=?');
